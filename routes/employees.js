25f6f7280a0ad9a2af3ecb162d59da885fb4ceee
Ashna Abraham <ashna.abraham@kainos.com>
2025-01-23 22:10:30 +0000

    prep for day 2

--- routes/employees.js
+++ routes/employees.js
@@ -1,23 +1,21 @@
 var express = require('express');
 var router = express.Router();
-const UserService = require('../services/employeeService');
-const userService = new UserService();
+const EmployeeService = require('../services/employeeService');
+const employeeService = new EmployeeService();
 
 
 
 router.get('/', (req,res)=>{
-    const users = userService.getAllUsers();
-
+    const employees = employeeService.getAllEmployees();
     console.log('here');
-    res.render('employees',{users})
+    res.render('employees',{employees})
 
-  
 });
 
 
 router.get('/:id', (req, res) => {
     console.log("Hello");
-    const employee = userService.getUserById(parseInt(req.params.id));
+    const employee = employeeService.getEmployeeById(parseInt(req.params.id));
     console.log(`here ${employee.name}`);
     if (!employee) return res.status(404).send('Employee not found');
     res.render('employee', { employee: employee })
--- services/employeeService.js
+++ services/employeeService.js
@@ -1,7 +1,6 @@
-// userService.js
 const fs = require('fs');
 
-class UserService {
+class EmployeeService {
     constructor() {
         this.filePath = "employees.json";
     }
@@ -16,58 +15,16 @@ class UserService {
             return [];
         }
     }
-
-    // Helper function to write users to JSON file
-    writeUsers(users) {
-        try {
-            fs.writeFileSync(this.filePath, JSON.stringify(users, null, 2), 'utf8');
-        } catch (err) {
-            console.error('Error writing users:', err);
-        }
-    }
-
-    // Get all users
-    getAllUsers() {
+    // Get all employees
+    getAllEmployees() {
         return this.readUsers();
     }
 
     // Get a employee by ID
-    getUserById(id) {
+    getEmployeeById(id) {
         const users = this.readUsers();
         return users.find(user => user.id === id);
     }
-
-    // Create a new user
-    createUser(newUser) {
-        const users = this.readUsers();
-        newUser.id = users.length ? users[users.length - 1].id + 1 : 1;
-        users.push(newUser);
-        this.writeUsers(users);
-        return newUser;
-    }
-
-    // Update a user by ID
-    updateUser(id, updatedUser) {
-        const users = this.readUsers();
-        const userIndex = users.findIndex(user => user.id === id);
-        if (userIndex === -1) return null;
-
-        updatedUser.id = id;
-        users[userIndex] = updatedUser;
-        this.writeUsers(users);
-        return updatedUser;
-    }
-
-    // Delete a user by ID
-    deleteUser(id) {
-        const users = this.readUsers();
-        const userIndex = users.findIndex(user => user.id === id);
-        if (userIndex === -1) return null;
-
-        const deletedUser = users.splice(userIndex, 1);
-        this.writeUsers(users);
-        return deletedUser[0];
-    }
 }
 
-module.exports = UserService;
+module.exports = EmployeeService;
