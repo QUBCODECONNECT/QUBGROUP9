ba28de12f0c45b3c63358f27680ba716e2721fae
Caitlin Fisher <cfisher12@qub.ac.uk>
2025-01-24 11:49:45 +0000

    edit, delete and create new employee

--- services/employeeService.js
+++ services/employeeService.js
@@ -15,6 +15,14 @@ class EmployeeService {
             return [];
         }
     }
+
+     writeUsers(employees) {
+            try {
+                fs.writeFileSync(this.filePath, JSON.stringify(employees, null, 2), 'utf8');
+            } catch (err) {
+                console.error('Error writing users:', err);
+            }
+        }
     // Get all employees
     getAllEmployees() {
         return this.readUsers();
@@ -22,8 +30,39 @@ class EmployeeService {
 
     // Get a employee by ID
     getEmployeeById(id) {
-        const users = this.readUsers();
-        return users.find(user => user.id === id);
+        const employees = this.readUsers();
+        return employees.find(employee => employee.id === id);
+    }
+    // Create a new user
+    createEmployee(newEmployee) {
+        const employees = this.readUsers();
+        newEmployee.id = employees.length ? employees[employees.length - 1].id + 1 : 1;
+        employees.push(newEmployee);
+        this.writeUsers(employees);
+        return newEmployee;
+    }
+
+    // Update a user by ID
+    updateEmployee(id, updatedEmployee) {
+        const employees = this.readUsers();
+        const userIndex = employees.findIndex(user => user.id === id);
+        if (userIndex === -1) return null;
+
+        updatedEmployee.id = id;
+        employees[userIndex] = updatedEmployee;
+        this.writeUsers(employees);
+        return updatedEmployee;
+    }
+
+    // Delete a user by ID
+    deleteEmployee(id) {
+        const employees = this.readUsers();
+        const userIndex = employees.findIndex(user => user.id === id);
+        if (userIndex === -1) return null;
+
+        const deletedUser = employees.splice(userIndex, 1);
+        this.writeUsers(employees);
+        return deletedUser[0];
     }
 }
 
